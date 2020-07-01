7e2733294012dad2a1282e67a564a789807620b1
arena <arena@DESKTOP-CLRPJ7S>
2020-06-30 22:02:54 -0500

    Initial Commit

---
+++ commons-model/src/main/java/com/checkout/commons/models/entity/Product.java
@@ -0,0 +1,69 @@
+package com.checkout.commons.models.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+
+	private Integer quantity;
+
+	private BigDecimal cost;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantiy) {
+		this.quantity = quantiy;
+	}
+
+	public BigDecimal getCost() {
+		return cost;
+	}
+
+	public void setCost(BigDecimal cost) {
+		this.cost = cost;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Product)) {
+			return false;
+		}
+		Product p = (Product) obj;
+		return this.id != null && this.id.equals(p.getId());
+	}
+
+}
---
+++ commons-model/src/test/java/com/checkout/commons/models/CommonsModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkout.commons.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
